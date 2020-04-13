9614cbcdb6f9f8c9c55bd5c0c8d515977060a479
snotling54 <guillaume.maljean@gmail.com>
2020-04-13 18:14:11 +0200

    spring boot admin

--- src/main/java/org/snot/springboottemplate/SpringBootTemplateApplication.java
+++ src/main/java/org/snot/springboottemplate/SpringBootTemplateApplication.java
@@ -1,9 +1,11 @@
 package org.snot.springboottemplate;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootTemplateApplication {
 
 	public static void main(String[] args) {
